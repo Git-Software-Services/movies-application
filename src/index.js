da7ad67f27c9f6a1315d74b3e35c19ef7d24684a
aziztalyahya <aziz.t.alyahya@gmail.com>
2018-12-20 17:07:22 -0600

    commiting work from end of day

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 /**
  * es6 modules and imports
  */
+import $ from "jquery";
 import sayHello from './hello';
-sayHello('World');
+console.log(sayHello("yo!"));
+document.getElementById("helloThere").innerHTML = (sayHello("World!"));
 
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
+import {getMovies} from './api.js';
+
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+      $("#movie" + id).text(`id#${id} - ${title} - rating: ${rating}`);
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
