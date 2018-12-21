dd3cbb1cbadf95493410f5d98257fd0be139a45b
aziztalyahya <aziz.t.alyahya@gmail.com>
2018-12-21 12:28:37 -0600

    halfway done with add movie function

--- src/index.js
+++ src/index.js
@@ -4,20 +4,49 @@
 import $ from "jquery";
 import sayHello from './hello';
 console.log(sayHello("yo!"));
-document.getElementById("helloThere").innerHTML = (sayHello("World!"));
+
 
 /**
  * require style imports
  */
 import {getMovies} from './api.js';
 
+const renderMovies = () => {
+    getMovies().then((movies) => {
+        $("#loading").css("display", "none");
+        $("#form").css("display", "block");
+        document.getElementById("helloThere").innerHTML = (sayHello("World!"));
+        movies.forEach(({title, rating, id}) => {
+            $(".container").append(`id#${id} - ${title} - rating: ${rating}<br>`);
+        });
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.')
+        console.log(error);
+    });
+};
+renderMovies();
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-      $("#movie" + id).text(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
+
+
+const addMovie =(movieTitle, movieRating) => {
+    const movieAdded = {title: movieTitle, rating: movieRating};
+    const url = '/api/movies';
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(movieAdded),
+    };
+    fetch(url, options)
+        .then(renderMovies())
+        .catch(alert("Error"));
+};
+
+$("#submit").click(function (e) {
+    e.preventDefault();
+    console.log($("#movieTitle").val() + " " + $("#movieRating").val())
+    var movieTitle = $("#movieTitle").val();
+    var movieRating = parseFloat($("#movieRating").val());
+    addMovie(movieTitle, movieRating);
 });
